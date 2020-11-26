a957742d095f0c16e860f3558ba1678f5da56858
Maria Theresa Henriques <mariatheresahenriques@pop-os.localdomain>
2020-11-26 18:19:12 -0300

    Selection Sort - comentario linha bug

--- src/ExeSelectionSort.c
+++ src/ExeSelectionSort.c
@@ -25,10 +25,13 @@ int main()
 } 
 
 /*
+-----------------------------------------
+ Codigo no GeeksforGeeks
+-----------------------------------------
 int main() 
 { 
     int arr[] = {64, 25, 12, 22, 11}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
+    int n = sizeof(arr)/sizeof(arr[0]);  // dando problema na adaptacao
     selectionSort(arr, n); 
     printf("Sorted array: \n"); 
     printArray(arr, n); 
